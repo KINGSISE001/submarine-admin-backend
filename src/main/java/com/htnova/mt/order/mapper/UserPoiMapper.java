49fe85bcff3df0fe13cf734cc71da990fcc8e78b
RISE <949092641@qq.com>
2024-01-24 09:12:53 +0800

    变更

--- src/main/java/com/htnova/mt/order/mapper/UserPoiMapper.java
+++ src/main/java/com/htnova/mt/order/mapper/UserPoiMapper.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.htnova.mt.order.dto.UserPoiDto;
 import com.htnova.mt.order.entity.UserPoi;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserPoiMapper extends BaseMapper<UserPoi> {
     IPage<UserPoi> findPage(IPage<Void> xPage, @Param("userPoiDto") UserPoiDto userPoiDto);
+
 
     List<UserPoi> findList(@Param("userPoiDto") UserPoiDto userPoiDto);
 }
